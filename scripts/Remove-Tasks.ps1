c50c29c0aa8ddc4b54134735bdab4fab4171bceb
WildByDesign <wildbydesign@live.ca>
2024-11-06 09:12:35 -0500

    Delete scripts directory
    - finally able to get rid of the external powershell scripts and added the related functionality directly into the main code

--- scripts/Remove-Tasks.ps1
+++
@@ -1,6 +0,0 @@
-﻿Get-ScheduledTask | where TaskPath -eq "\AppControlTray\" | Unregister-ScheduledTask -Confirm:$false
-
-$scheduleObject = New-Object -ComObject Schedule.Service
-$scheduleObject.connect()
-$rootFolder = $scheduleObject.GetFolder("\")
-$rootFolder.DeleteFolder("AppControlTray",$null)
